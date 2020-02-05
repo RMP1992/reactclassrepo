c31ca138ba723a3408998cfaf53ffee8ea80a1af
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-02-05 10:59:15 +0000

    class with an if statement

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state={
+    counter: 0,
+    colour: "Burgundi",
+  }
+  greeting = ()=>{
+    alert('hello')
+  }
+  increment =() =>{
+    this.setState({
+      counter: this.state.counter +1
+    });
+  }
+  decrement =() =>{
+    this.setState({
+      counter: this.state.counter -1
+    })
+  }
+  // changeColour = ()=>{
+  //   this.setState({
+  //   colour: this.state.colour = "Crimson"
+        
+  //   })
+  // }
+  changeColour = ()=>{
+    let colour = this.state.colour; //Crimson
+    if(colour == "Burgundi"){
+      this.setState({
+        colour: "Crimson"
+      })
+    }else {
+      this.setState({
+        colour: "Burgundi"
+      })
+    }
+  }
+  render(){
+    return(
+      <div>
+      <h1>My first Web page with react classes</h1>
+      <h1>My current counter is {this.state.counter}</h1>
+      <h1>My favourite colour is {this.state.colour}</h1>
+      <button onClick= {this.greeting}>Submit</button>
+      <button onClick= {this.increment}>Increment the counter</button>
+      <button onClick= {this.decrement}>Decrement the counter</button>
+      <button onClick= {this.changeColour}>Change colour</button>
+      </div>
+    )
+  }
 }
 
 export default App;
